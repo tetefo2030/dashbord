4e3f15b602f918873d7d0704cde83cac2dad7950
tetefo2030 <tetefo2030@gmail.com>
2026-08-01 17:20:17 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
+
+<script>
+  var firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "atyaf-9e35f.firebaseapp.com",
+    databaseURL: "https://atyaf-9e35f-default-rtdb.firebaseio.com/",
+    projectId: "atyaf-9e35f",
+    storageBucket: "atyaf-9e35f.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+</script>
